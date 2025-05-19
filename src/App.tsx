4e8e8a994bec3fa1e54a85980dea195af3a8240e
Luky Setiawan <unluckly17@gmail.com>
2025-05-19 14:47:54 +0700

    [update] fix scroll behavior (#2)
    Co-authored-by: luky <luzion1508@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import { FooterPlexicus } from "./components/Footer"
 function App() {
 
   return (
-    <>
+    <div className="h-screen bg-black">
       <div className='flex'>
         <NavbarPlexicus />
       </div>
+      <div className="h-screen"></div>
       <div className="mt-24">
         <FooterPlexicus/>
       </div>
-    </>
+    </div>
   )
 }
 
