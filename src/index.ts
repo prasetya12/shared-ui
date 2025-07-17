c6b8db06bc7b54bbc59fd0c2d8678dd877205f9b
Slackluky <unluckly17@gmail.com>
2025-07-18 00:03:06 +0700

    [update]Import the main CSS file to apply global styles.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './index.css'
 export {NavbarPlexicus} from "./components/Navbar"
 export {FooterPlexicus} from "./components/Footer"
 export { SelectLanguage } from "./components/ui/select-language"
