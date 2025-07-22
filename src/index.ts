e69085ab852e00eafd7862b0aa67fbdad362d2ac
Slackluky <unluckly17@gmail.com>
2025-07-22 22:18:52 +0700

    Refactor: Import GoogleAnalytics component directly from GTM component file.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import GoogleAnalytics from './components/GTM'
-import { gtmTrackingCode as trackingId} from './constants/Gtm';
 
-const gtm = GoogleAnalytics({trackingId})
+
 import './index.css'
 export {NavbarPlexicus} from "./components/Navbar"
 export {FooterPlexicus} from "./components/Footer"
 export { SelectLanguage } from "./components/ui/select-language"
 
-export {gtm}
+export {GoogleAnalytics} from './components/GTM'
