1f78490ef94a278e6218943349841ffb98f30154
Slackluky <unluckly17@gmail.com>
2025-07-22 21:34:33 +0700

    [update] Export the Google Tag Manager component for external use.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+import GoogleAnalytics from './components/GTM'
+import { gtmTrackingCode as trackingId} from './constants/Gtm';
+
+const gtm = GoogleAnalytics({trackingId})
 import './index.css'
 export {NavbarPlexicus} from "./components/Navbar"
 export {FooterPlexicus} from "./components/Footer"
 export { SelectLanguage } from "./components/ui/select-language"
+
+export {gtm}
