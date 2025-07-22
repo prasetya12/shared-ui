731e8e7e4ea7cc63da1786b96ae31b33e3595a77
Slackluky <unluckly17@gmail.com>
2025-07-22 23:35:41 +0700

    [update] Re-export `useTrackClick` hook from the index file.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 
 
-import useTrackClick from './hooks/gtm-event-tracker'
 import './index.css'
 export {NavbarPlexicus} from "./components/Navbar"
 export {FooterPlexicus} from "./components/Footer"
 export { SelectLanguage } from "./components/ui/select-language"
 
 export {GoogleAnalytics} from './components/GTM'
-export {useTrackClick}
+export {useTrackClick} from './hooks/gtm-event-tracker'
