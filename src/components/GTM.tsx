91b96fe1549ab7f34c714895fde3abac4c4ae9c8
Slackluky <unluckly17@gmail.com>
2025-07-30 01:47:43 +0700

    [update] Refactor Google Analytics component to use script tags and improve readability.

--- src/components/GTM.tsx
+++ src/components/GTM.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { gtmTrackingCode } from '../constants/Gtm';
 /**
  * GoogleAnalytics Component
@@ -11,38 +11,25 @@ import { gtmTrackingCode } from '../constants/Gtm';
  * This ID will be used to configure gtag.
  */
 export const GoogleAnalytics: React.FC<{ trackingId: string }> = ({trackingId = gtmTrackingCode}) => {
-  useEffect(() => {
- 
+  return (
+    <>
+      {/* External gtag.js script */}
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`} />
 
-    // Create the inline script element for dataLayer configuration
-    const inlineScript = document.createElement('script');
-    // Set the script as inline to avoid issues with module systems
-    inlineScript.setAttribute('is:inline', ''); // This attribute is for Astro, but harmless in React.
-                                            // For pure React, this attribute isn't strictly necessary for functionality.
-    inlineScript.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      gtag('config', '${trackingId}');
-    `;
-    document.head.prepend(inlineScript);
-
-
-       // Create the external gtag.js script element
-       const externalScript = document.createElement('script');
-       externalScript.async = true;
-       externalScript.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
-       document.head.prepend(externalScript);
-    // Cleanup function: Remove the scripts when the component unmounts
-    return () => {
-      document.head.removeChild(externalScript);
-      document.head.removeChild(inlineScript);
-    };
-  }, [trackingId]); // Re-run effect if trackingId changes
-
-  // This component does not render any visible elements, so it returns null.
-  return null;
+      {/* Inline script for dataLayer configuration */}
+      <script
+        is:inline // This attribute is for Astro, but harmless in React.
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', '${trackingId}');
+          `,
+        }}
+      />
+    </>
+  );
 };
-
